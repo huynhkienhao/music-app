d4910ff4d3d11832395afe15b7c09f9647293ff0
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-13 22:39:51 +0700

    Client Bắt Lỗi Token Từ Server Trả Về

--- Client/src/TemplatesHTML/assets/js/admin_page.js
+++ Client/src/TemplatesHTML/assets/js/admin_page.js
@@ -1,6 +1,18 @@
-// Call API get list users
-axios.get('http://localhost:4000/auth/admin/user', {
-    headers: {
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+
+
+getListUser = async () => {
+    try {
+        const configHeader = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        }
+        // Call API get list users
+        const response = await axios.get('http://localhost:4000/auth/admin/user', configHeader);
+    } catch (error) {
+
     }
-})
+
+}
+
+getListUser();
