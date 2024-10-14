11caf7e9cbb99a01732e9d53cbc8122692a743c2
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-14 14:10:59 +0700

    get list users

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -1,13 +1,22 @@
 const jwt = require('jsonwebtoken');
+const userModel = require('../Models/UserModel');
 
-const getListUser = (req, res) => {
+const getListUser = async (req, res) => {
     // 1. Get token from Client
     const bearerHeader = req.headers['authorization'];
     const accessToken = bearerHeader.split(' ')[1];
 
-    // 2. Verify Token
-    const decodeJwt = jwt.verify(accessToken, process.env.SECRET_JWT);
-    console.log(decodeJwt);
+
+    try {
+        // 2. Verify Token
+        const decodeJwt = jwt.verify(accessToken, process.env.SECRET_JWT);
+        if (decodeJwt) {
+            const users = await userModel.find();
+            res.status(200).send(users);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const userDetail = (req, res) => {
