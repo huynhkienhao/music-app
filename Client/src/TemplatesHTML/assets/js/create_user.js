a13853dc6f0e0ee980a3935c7d02d9b270d17ba5
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-16 15:34:10 +0700

    Client Send Api Create User

---
+++ Client/src/TemplatesHTML/assets/js/create_user.js
@@ -0,0 +1,28 @@
+handleSubmitAddUser = async () => {
+    try {
+        // 1. Lấy giá trị trên form register
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const role = document.getElementById('role').value;
+
+        console.log(`username: ${username}`);
+        console.log(`email: ${email}`);
+        console.log(`password: ${password}`);
+        console.log(`role: ${role}`);
+
+        // 2. Gửi giá trị từ client đến server
+        const response = await axios.post('api/auth/user/create', {
+            username: username,
+            email: email,
+            password: password,
+            role: role
+        })
+
+        if (response.status === 200) {
+
+        }
+    } catch (error) {
+        // log error
+    }
+}
